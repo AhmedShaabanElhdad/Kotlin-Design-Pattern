c389dc116fcabb5e648e9cd6b3402c19c8afacdd
AhmedShaabanElhdad <ahmed.shaaban.elhdah@gmail.com>
2020-08-05 18:06:13 +0200

    SharedPRef  singleton design pattern

--- app/src/main/java/com/example/kotlindesignpattern/singleton/PrefHelper.kt
+++ app/src/main/java/com/example/kotlindesignpattern/singleton/PrefHelper.kt
@@ -3,14 +3,14 @@ package com.example.kotlindesignpattern.singleton
 import android.content.Context
 import android.content.SharedPreferences
 
-object PrefsHelper {
+class PrefsHelper private constructor(context: Context){
 
     private lateinit var prefs: SharedPreferences
 
-    private const val PREFS_NAME = "users"
+    private val PREFS_NAME = "users"
 
-    const val ID_USER = "id_user"
-    const val TOKEN = "token"
+    val ID_USER = "id_user"
+    val TOKEN = "token"
 
     fun init(context: Context) {
         prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
@@ -37,6 +37,35 @@ object PrefsHelper {
         with(prefsEditor) {
             putLong(key, value)
             commit()
+        }
+    }
+
+    companion object : SingletonHolder<PrefsHelper, Context>(::PrefsHelper)
+
+
+
+}
+
+open class SingletonHolder<out T: Any, in A>(creator: (A) -> T) {
+    private var creator: ((A) -> T)? = creator
+    @Volatile private var instance: T? = null
+
+    fun getInstance(arg: A): T {
+        val checkInstance = instance
+        if (checkInstance != null) {
+            return checkInstance
+        }
+
+        return synchronized(this) {
+            val checkInstanceAgain = instance
+            if (checkInstanceAgain != null) {
+                checkInstanceAgain
+            } else {
+                val created = creator!!(arg)
+                instance = created
+                creator = null
+                created
+            }
         }
     }
 }
